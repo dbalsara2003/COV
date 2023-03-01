309906bb4cb5a7aa0ebd17b731f316ef1e77441f
vycodin <kbyin2@gmail.com>
2023-03-01 12:35:32 -0800

    File upload set up, beginning of csv parsing file written as well

---
+++ formatfile.py
@@ -0,0 +1,5 @@
+##I don't really know how we want the data to be parsed so if someone could work on this with me would be very helpful :P
+import csv
+def csvParse(file):
+    print('oogabooga :)')
+    pass
--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from formatfile import csvParse
 app = Flask(__name__)
 
 # This is the home page thats in templates folder
@@ -13,6 +14,14 @@ def manual():
 @app.route('/check_manual')
 def check_manual_page():
     return "This page will be for inputting values for a single row needing to be estimated."
+
+@app.route('/upload_file', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['filename']
+        csvParse(f)
+        return redirect('http://localhost:8080/', code=200)
+
 
 #this needs to change to port 80 when we deploy on docker
 if __name__ == '__main__':
