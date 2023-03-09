9bf75c4dd025212538ff407571e0dfeeebc36de4
petervanderhook <petervanderhook@gmail.com>
2023-03-09 11:36:57 -0800

    Updated delimiter change script for new directory structure. Removed duplicate files. Created append_polygon_to_storefront script for adding a storefront's property parcel polygon to the storefront rows.

--- scripts/convert_csv_delimiter.py
+++ scripts/convert_csv_delimiter.py
@@ -1,11 +1,12 @@
 import chardet
-
+old_del = ','
+new_del = ';'
 new_f = []
-rawdata = open("./sample_storefront_inventory.csv", 'rb').read()
+rawdata = open(f"./data/sample_storefront_inventory.csv", 'rb').read()
 result = chardet.detect(rawdata)
 enc = result['encoding']
-with open("./sample_storefront_inventory.csv", mode="r+", encoding=enc) as f:
+with open("./data/sample_storefront_inventory.csv", mode="r+", encoding=enc) as f:
     for line in f:
-        new_f.append(str(line).replace("\t", ","))
-with open("./output.csv", mode="w+", encoding="utf-8") as f:
+        new_f.append(str(line).replace(old_del, new_del))
+with open("./data/sample_storefront_inventory.csv", mode="w+", encoding="utf-8") as f:
     f.writelines(new_f)
